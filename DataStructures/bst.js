aff74e341ed48cd54e7279dcaba960a5b4fdcd26
David Okeke <iamdavonokeke@gmail.com>
2022-08-18 00:58:45 +0100

    adding remove and ispresent method to Binary Search Tree

--- DataStructures/bst.js
+++ DataStructures/bst.js
@@ -7,25 +7,26 @@ class Node {
 }
 
 class Tree {
-    constructor(root){
+    constructor(){
         this.root= null
     }
-    add = function(value){
-        if (this.root = null){
-          return  this.root = new Node(value)
+    add(value){
+        if (this.root == null){
+          this.root = new Node(value)
+          return
         }
         else{
             const searchTree = function(node){
                 if (value < node.data){
-                    if (node.data.left == null){
-                     node.data.left = new Node(value)
+                    if (node.left == null){
+                     node.left = new Node(value)
                      return;
-                    }else if(node.data.left!= null){
+                    }else if(node.left!= null){
                         return searchTree(node.left)
                     }
                 }else if(value > node.data){
                     if (node.right == null){
-                        node.right == new Node(value)
+                        node.right = new Node(value)
                         return;
                     }else if (node.right != null){
                         return searchTree(node.right)
@@ -38,15 +39,39 @@ class Tree {
         }
 
     }
-    remove = function(node){
-        removenode = function(root){
-            if(root == null){
+    remove = function(data){
+        removenode = function(node, data){
+            if(node == null){
                 return null
-            }else if(root.left == null && root.right == null){
-                root == null
-                return;
+            }
+            if (node.data == data){
+                if(node.left == null && node.right == null){
+                     node.data = null
+                     return
+                }
+                if(node.left == null){
+                    node.data = node.right
+                }
+                if(node.right == null){
+                    node.data = node.left
+                }
+                var tempNode = node.right
+                while(tempNode!=null){
+                    tempNode = tempNode.left
+                }
+                node.data = tempNode.data
+                node.right = removeNode(tempNode.data, data)
+                return node
+            }
+            else if(node.data > data){
+                node.right = removeNode(node.right, data)
+                return
+            }
+            else{
+                node.left = removeNode(node.left, data)
             }
         }
+        this.root = removeNode(this.root, data);
     }
     findMin = function(){
        let current = this.root
@@ -67,13 +92,29 @@ class Tree {
         while (current){
             if (value == current.data){
                 return true
-            }else if( current.data > value){
-                current = current.right
-            }else if ( current.data < value){
+            }else if( value < current.data){
                 current = current.left
+            }else {
+                current = current.right
             }
-            return false;
+        }
+        return false;
+    }
+    printTree = function(){
+        if(this.root == null){
+            return null
+        }else{
+            var result = []
+            var printNode = function(node){
+                node.left && printNode (node.left)
+                result.push(node.data)
+                node.right && printNode (node.right)
+            }
+            printNode(this.root)
+            return result
         }
     }
 
 }
+
+module.exports = Tree
--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const Stack  = require("./DataStructures/stack");
 const Queue  = require("./DataStructures/queue");
-const BinarySearchTree  = require("./DataStructures/bts");
+const BinarySearchTree  = require("./DataStructures/bst");
 
+const Bst = new BinarySearchTree
+
+Bst.add(8)
+Bst.add(7)
+Bst.add(1)
+Bst.add(3)
+Bst.add(2)
+console.log(Bst.printTree())
+console.log(Bst.isPresent(7))
